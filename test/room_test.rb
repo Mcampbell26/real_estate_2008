b3de3a22506e7958549b824dbd0c46facc85b658
Megan Campbell <megan.campbell26@yahoo.com>
2020-08-26 09:21:48 -0600

    Initial commit

--- test/room_test.rb
+++ test/room_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/room'
+require 'pry'
 
 class RoomTest < Minitest::Test
   def test_it_exists
@@ -9,15 +10,16 @@ class RoomTest < Minitest::Test
   end
 
   def test_it_has_a_category
-    skip
+    # skip
     room = Room.new(:bedroom, 10, '13')
     assert_equal :bedroom, room.category
   end
 
   def test_it_can_get_area
-    skip
+    # skip
     room1 = Room.new(:bedroom, 10, '13')
     room2 = Room.new(:living_room, 15, '12')
+# binding.pry
     assert_equal 130, room1.area
     assert_equal 180, room2.area
   end
